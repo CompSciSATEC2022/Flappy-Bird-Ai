249970f3b60eb2e5239061beadbb631589c61666
CommanderKV <kylervisser@gmail.com>
2022-03-22 19:16:24 -0400

    Adding starting code

--- main.py
+++ main.py
@@ -1,1 +1,64 @@
 import pygame
+import os
+
+# Set up global vairables
+WIN_SIZE = (1000, 500) # The size of the game window
+WIN = pygame.display.set_mode(WIN_SIZE) # The pygame window itself
+FPS = 60 # Set the FPS the game will run at
+
+# Get all of the images loaded
+NON_BIRD_IMGS = [
+    pygame.image.load (os.path.join (os.getcwd (), "imgs", "pipe.png" )), # Pipe
+    pygame.image.load (os.path.join (os.getcwd (), "imgs", "base.png" )), # A base section
+    pygame.image.load (os.path.join (os.getcwd (), "imgs", "bg.png"   ))  # Background
+]
+
+BIRD_IMAGES = [pygame.transform.scale2x (pygame.image.load (os.path.join ("imgs", "bird" + str (number) + ".png"))) for number in range (1, 4)]
+
+
+
+# ----------------------------
+#     Drawing the window
+# ----------------------------
+#   This will handle all the 
+#     drawing on the screen
+# ----------------------------
+def draw_window (window):
+    # Fill the background with black
+    window.fill ( (0, 0, 0) )
+
+    # Update the screen
+    pygame.display.update ()
+
+
+
+# ------------------------
+#     Main function
+# ------------------------
+#   This will handle all
+#        game logic
+# ------------------------
+def main ():
+
+    # Make a clock to control FPS
+    clock = pygame.time.Clock ()
+    
+
+    # Main game loop
+    run = True
+    while (run):
+
+        # Control fps
+        clock.tick (FPS)
+
+        # For each event that happens do
+        for event in pygame.event.get ():
+
+            # If the X button was pressed exit
+            if (event.type == pygame.QUIT):
+                run = False
+
+
+# If this is the main file that is being run then
+if (__name__ == "__main__"):
+    main ()
